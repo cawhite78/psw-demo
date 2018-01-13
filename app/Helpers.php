11fefa7a84027bc38b85d5ec6b85312d1a1482b0
Corey White <coreyjr@gmail.com>
2018-01-12 17:01:02 -0700

    bunch more changes

---
+++ app/Helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+// truncate string at word
+function truncateContent($string, $limit, $break = ".", $pad = "...")
+{
+
+    if (strlen($string) <= $limit) {
+        return $string;
+    }
+
+    if (false !== ($max = strpos($string, $break, $limit))) {
+
+        if ($max < strlen($string) - 1) {
+
+            $string = substr($string, 0, $max).$pad;
+        }
+    }
+
+    return $string;
+}
+
+
+function getBrandLogo($brand)
+{
+    $imageName = strtolower(str_replace(' ','-', $brand)).'.jpg';
+    return '/images/brands/'.$imageName;
+}
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,10 +11,64 @@
 
     <div class="container content-area">
         <div class="row">
-            <div class="col-4" style="border:1px solid #efefef">{{print_r($featured->toArray())}}
+            <div class="col-12 col-md-8 col-lg-8">
+
+
+                <div class="home-featured p-4">
+                    <h3>Featured Products</h3>
+                    <div class="mb-4 mt-4">
+                    @foreach($featured as $product)
+                        <h4>{{$product['name']}}</h4>
+                        <div class="row mb-5">
+                            <div class="col-2">
+                                <img src="{{$product['primary_image']}}" width="100%"/>
+                            </div>
+                            <div class="col-10">
+                                <p>{{truncateContent($product['description'] ,100, '.', ' ...')}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                    </div>
+                </div>
+
             </div>
-            <div class="col-4" style="border:1px solid #efefef">{{print_r($brands->toArray())}}</div>
-            <div class="col-4" style="border:1px solid #efefef">{{print_r($categories->toArray())}}</div>
+
+            <div class="col-12 col-md-4 col-lg-4">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="home-brands p-4">
+                            <h3>Popular Brands</h3>
+
+                            <div class="row">
+                            @foreach($brands->toArray() as $brand)
+
+                                    <div class="col-4 mt-3 mt-2" style="text-align: center;">
+                                        <h6>{{$brand['brand']}}</h6>
+                                        <img src="{{getBrandLogo($brand['brand'])}}" style="width:100%; height:auto;"/>
+                                    </div>
+                            @endforeach
+                            </div>
+
+                        </div>
+
+                        <div class="home-categories p-4">
+                            <h3>Product Categories</h3>
+                            <div class="row">
+
+
+                            @foreach($categories->toArray() as $category)
+
+                                    <div class="col-4 mt-3 mt-2" style="text-align: center;">
+                                        <button type="button" class="btn btn-outline-primary">{{$category['type']}}</button>
+                                    </div>
+                                @endforeach
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 
